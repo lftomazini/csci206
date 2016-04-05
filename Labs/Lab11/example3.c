e5c59cecb1f4324305088ebf72d45d0ee1d6679a
Luis Felipe <lffct001@bucknell.edu>
2016-04-05 00:29:58 -0400

    new files

--- Labs/Lab11/example3.c
+++ Labs/Lab11/example3.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void function(int a, int b, int c) {
-	char buffer1[5];
-	char buffer2[10];
-	int *ret;
+void nop(){
+}
 
-	ret = (int*)(buffer1 + 24);
-	(*ret) += 8;
+void function(int a, int b, int c) {
+    char buffer1[5];
+    char buffer2[10];
+    int *ret;
+
+    nop();
+    ret = (int*)(buffer1 + 24);
+    (*ret) += 8;
 }
 
 void main() {
-	int x;
+    int x;
 
-	x = 0;
-	function(1,2,3);
-	x = 1;
-	printf("%d\n",x);
+    x = 0;
+    function(1,2,3);
+    x = 1;
+    printf("%d\n",x);
 }
